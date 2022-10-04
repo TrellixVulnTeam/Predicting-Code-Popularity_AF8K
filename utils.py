7b43d13dac88df84bbd8a84b793a38b6afe73009
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 04:32:23 +0000

    Adding numeric_owner as keyword arguement

--- utils.py
+++ utils.py
@@ -176,7 +176,7 @@ def download(repo):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tarball)
